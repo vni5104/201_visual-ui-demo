db0f4b65b7256ce60868ad0ba78275c953addc09
Vicky <vickyn5104@gmail.com>
2025-09-17 22:29:45 -0400

    added Stepper and Slider control

--- ExperimentWithControls/ExperimentWithControls/MainPage.xaml.cs
+++ ExperimentWithControls/ExperimentWithControls/MainPage.xaml.cs
@@ -20,5 +20,20 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnteredText.Text = e.NewTextValue;
+        }
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            StepperValue.Text = e.NewValue.ToString();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderValue.Text = e.NewValue.ToString();
+        }
     }
 }
