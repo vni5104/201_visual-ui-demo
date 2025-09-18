75792f6bb2bae3a6bcafbdead5a54d7e91c60a1c
Vicky <vickyn5104@gmail.com>
2025-09-17 21:24:02 -0400

    .Net MAUI app initialization

---
+++ ExperimentWithControls/ExperimentWithControls/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ExperimentWithControls")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ExperimentWithControls.Pages")]
