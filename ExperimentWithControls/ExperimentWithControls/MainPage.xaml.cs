6081ff7120fee8d6e4f7aa643c4536ab809e8a63
Vicky <vickyn5104@gmail.com>
2025-09-17 22:53:17 -0400

    added Picker control

--- ExperimentWithControls/ExperimentWithControls/MainPage.xaml.cs
+++ ExperimentWithControls/ExperimentWithControls/MainPage.xaml.cs
@@ -7,6 +7,11 @@
         public MainPage()
         {
             InitializeComponent();
+
+            BirdPicker.ItemsSource = new string[]
+            {
+                "Duck", "Pigeon", "Penguin", "Ostrich", "Owl"
+            };
         }
 
         private void OnCounterClicked(object? sender, EventArgs e)
@@ -34,6 +39,15 @@
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             SliderValue.Text = e.NewValue.ToString();
+        }
+
+        private void AddBird_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(Birds.Text))
+            {
+                Birds.Text = Birds.Text + Environment.NewLine;
+            }
+            Birds.Text += BirdPicker.SelectedItem;
         }
     }
 }
